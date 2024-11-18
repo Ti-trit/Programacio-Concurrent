92c231928f308a8fc6b487dcaba80a2bfddd2c79
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-11-18 09:55:26 +0100

    changed num_refrescs>1 to num_refrescs>0

--- exercici_practic2/src/def_monitor.adb
+++ exercici_practic2/src/def_monitor.adb
@@ -3,7 +3,7 @@ with Text_IO; use Text_IO;
 
 package body def_monitor is
    
-   --funciÛ que genera un numero aleatori en el rang [MIN, MAX]
+   --funci√≥ que genera un numero aleatori en el rang [MIN, MAX]
    function randomNumber(MIN, MAX:Integer) return Integer is
          type range_type is new Integer range MIN..MAX;
          package Rand_Int is new Ada.Numerics.Discrete_Random(range_type);
@@ -38,8 +38,8 @@ package body def_monitor is
          CLIENTS := randomNumber(MIN, MAX);
          REPOSADORS := randomNumber(MIN, MAX);
          num_clients_actius:= CLIENTS;
-         Put_Line("SimulaciÛ amb " & CLIENTS'img & " clients i " & REPOSADORS'img & " reposadors");
-         Put_Line("********** La m‡quina est‡ preparada");
+         Put_Line("Simulaci√≥ amb " & CLIENTS'img & " clients i " & REPOSADORS'img & " reposadors");
+         Put_Line("********** La m√†quina est√† preparada");
 
       end initialize_values;
 
@@ -49,25 +49,25 @@ package body def_monitor is
          quantitat: Integer:= Max_refrescs-NUM_REFRESCS;
       begin 
          
-         --Si la m‡quina no est· ple i encara hi ha clients actius
+         --Si la m√†quina no est√° ple i encara hi ha clients actius
           
         if (NUM_REFRESCS<Max_refrescs and num_clients_actius>0) then 
         
        Put_Line("++++++++++ El reposador " & id'Img & " reposa " & quantitat'Img & " refrescs, ara n'hi ha " & Max_refrescs'Img);
-         NUM_REFRESCS:= Max_refrescs;--omplir la maquina fins al m‡xim
+         NUM_REFRESCS:= Max_refrescs;--omplir la maquina fins al m√†xim
          end if;
          
          
       end posarRefrescs;
       
       
-   entry consumir (nom: in nomClient; maxConsumacions: in Integer; cons_fetes: in out Integer)  when (NUM_REFRESCS>1)  is
+   entry consumir (nom: in nomClient; maxConsumacions: in Integer; cons_fetes: in out Integer)  when (NUM_REFRESCS>0)  is
       begin 
        
          cons_fetes:= cons_fetes+1;
          NUM_REFRESCS:= NUM_REFRESCS-1;
      
-         Put_Line("---------- " & To_String(nom) & " agafa el refresc " & cons_fetes'Img & "/" & maxConsumacions'Img & " a la m‡quina en queden " &NUM_REFRESCS'Img);
+         Put_Line("---------- " & To_String(nom) & " agafa el refresc " & cons_fetes'Img & "/" & maxConsumacions'Img & " a la m√†quina en queden " &NUM_REFRESCS'Img);
        
       
    end consumir;
