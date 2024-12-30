2bf48d30fcd7a85afd2f46112af5022b9133e987
Ti-trit <115530714+Ti-trit@users.noreply.github.com>
2024-12-30 21:39:57 +0100

    Plantillas básicas para emprezar

---
+++ Plantillas/plantillaJava.java
@@ -0,0 +1,47 @@
+
+/**
+ * Plantilla generica de programació concurrent en Java
+ * @author Khaoula Ikkene
+ * TOTS ELS TIPUS PRIMITIUS SÓN ATOMICS.
+ * LONG I DOUBLE HO PODEN SER SI ES DECLAREN VOLATILS.
+ */
+public class plantillaJava implements Runnable{
+    static final int THREADS= 3;
+    int id;
+
+    @Override
+    public void run() {
+
+        // TODO Auto-generated method stub
+        System.out.println("som el thread"+ id); 
+   }
+
+    public plantillaJava(int id){
+        this.id = id;
+    }
+
+
+
+
+
+
+    // La invocació de start() i join() requereix la gestió de l'excepció InterruptedException
+    public static void main(String args[]) throws InterruptedException{
+        Thread[] threads = new Thread[THREADS];
+        int i;
+
+        for (i = 0; i < THREADS; i++) {
+            threads[i] = new Thread(new plantillaJava(i));
+            threads[i].start();
+        }
+
+        for (i = 0; i < THREADS; i++) {
+            threads[i].join();
+        }
+
+    }
+
+
+
+
+}
